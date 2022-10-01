9e6767c9a1e40d92b655b02eaa008007f6c77b55
Siddharth Sahu <siddharth201820@gmail.com>
2022-10-02 01:47:15 +0530

    grid changes

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const tileWidth = canvas.width / 4;
+const tileHeight = canvas.height / 4;
 
-for (let i = 1; i < 4; ++i) {
-  ctx.beginPath();
-  ctx.moveTo(tileWidth * i, 0);
-  ctx.lineTo(tileWidth * i, canvas.height);
-  ctx.stroke();
+ctx.lineWidth = "2";
+
+for (let x = 1; x < 4; ++x) {
+  for (let y = 1; y < 4; ++y) {
+    ctx.translate(1, 1);
+    ctx.moveTo(-1, tileHeight * x);
+    ctx.lineTo(canvas.width + 1, tileHeight * x);
+    ctx.moveTo(tileWidth * y, -1);
+    ctx.lineTo(tileWidth * y, canvas.height + 1);
+    ctx.stroke();
+    ctx.translate(-1, -1);
+  }
 }
