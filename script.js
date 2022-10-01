128b644db1a15884d602c944b98ce4371cdf8a12
Siddharth Sahu <siddharth201820@gmail.com>
2022-10-02 00:20:39 +0530

    minor UI changes

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const tileWidth = canvas.width / 4;
 
-const tileWidth = window.innerWidth/4;
-
-ctx.strokeStyle = '#000000';
-for(let i=1;i<4;++i){
-    ctx.moveTo(tileWidth*i,0);
-    ctx.lineTo(tileWidth*i,canvas.height);
-    ctx.stroke();
+for (let i = 1; i < 4; ++i) {
+  ctx.beginPath();
+  ctx.moveTo(tileWidth * i, 0);
+  ctx.lineTo(tileWidth * i, canvas.height);
+  ctx.stroke();
 }
