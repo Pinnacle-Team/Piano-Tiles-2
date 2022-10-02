18fe00f71ae325df0e6f68bdffc26a883793177e
Siddharth Sahu <siddharth201820@gmail.com>
2022-10-02 13:03:51 +0530

    update function

--- script.js
+++ script.js
@@ -8,49 +8,65 @@ const HEIGHT = canvas.height;
 const tileWidth = WIDTH / 4;
 const tileHeight = HEIGHT / 4;
 
-
-for (let x = 1; x < 4; ++x) {
-  for (let y = 1; y < 4; ++y) {
-    // ctx.translate(1, 1);
-    ctx.moveTo(-1, tileHeight * x);
-    ctx.lineTo(WIDTH + 1, tileHeight * x);
-    ctx.moveTo(tileWidth * y, -1);
-    ctx.lineTo(tileWidth * y, HEIGHT + 1);
-    ctx.stroke();
-    // ctx.translate(-1, -1);
+function drawGrid() {
+  for (let x = 1; x < 4; ++x) {
+    for (let y = 1; y < 4; ++y) {
+      // ctx.translate(1, 1);
+      ctx.moveTo(-1, tileHeight * x);
+      ctx.lineTo(WIDTH + 1, tileHeight * x);
+      ctx.moveTo(tileWidth * y, -1);
+      ctx.lineTo(tileWidth * y, HEIGHT + 1);
+      ctx.stroke();
+      // ctx.translate(-1, -1);
+    }
   }
 }
-
 
 var tiles = [];
 
 //add new row function
-function addNewRow(){
-  let blackIndex = Math.floor(Math.random()*3);
-  for(let i=0;i<4;++i){
-    if(i===blackIndex){
+function addNewRow() {
+  let blackIndex = Math.floor(Math.random() * 4);
+  for (let i = 0; i < 4; ++i) {
+    if (i === blackIndex) {
       tiles.push(0);
-    }else{
+    } else {
       tiles.push(1);
     }
   }
 }
 
-function drawTiles(){
+function drawTiles() {
+  
+  let alphabets=['A','S','D','F'];
   for(let i=0;i<tiles.length;++i){
     let xpos = (i%4)*tileWidth;
     let ypos = Math.floor(i/4)*tileHeight;
 
-    ctx.fillStyle ='#000000'
+    ctx.strokeStyle = '#FFFFFF';
     if(tiles[i]==0){
+      ctx.fillStyle ='#000000';
       ctx.fillRect(xpos,ypos,tileWidth,tileHeight);
-    }
-  }
+      ctx.fillStyle ='#FFFFFF';
+      ctx.font = '50px Arial';
+      ctx.fillText(alphabets[i%4],xpos+33,ypos+85);
 }
 
-addNewRow();
-addNewRow();
-addNewRow();
-addNewRow();
-drawTiles();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGrid();
+  addNewRow();
+  addNewRow();
+  addNewRow();
+  addNewRow();
+  drawTiles();
+}
+
+function update() {
+  requestAnimationFrame(update);
+  // Draw everything
+  draw();
+}
+
+draw();
 console.log(tiles);
