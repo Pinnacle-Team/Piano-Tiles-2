4c37eec3712b87b083b4f9e35b5fb43474a2ae0f
nayakvikas <nvikas1211@gmail.com>
2022-10-02 11:35:21 +0530

    adding new row function added

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-
-const tileWidth = canvas.width / 4;
-const tileHeight = canvas.height / 4;
-
 ctx.lineWidth = "2";
+
+//global variables
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+const tileWidth = WIDTH / 4;
+const tileHeight = HEIGHT / 4;
+
 
 for (let x = 1; x < 4; ++x) {
   for (let y = 1; y < 4; ++y) {
-    ctx.translate(1, 1);
+    // ctx.translate(1, 1);
     ctx.moveTo(-1, tileHeight * x);
-    ctx.lineTo(canvas.width + 1, tileHeight * x);
+    ctx.lineTo(WIDTH + 1, tileHeight * x);
     ctx.moveTo(tileWidth * y, -1);
-    ctx.lineTo(tileWidth * y, canvas.height + 1);
+    ctx.lineTo(tileWidth * y, HEIGHT + 1);
     ctx.stroke();
-    ctx.translate(-1, -1);
+    // ctx.translate(-1, -1);
   }
 }
+
+
+var tiles = [];
+
+//add new row function
+function addNewRow(){
+  let blackIndex = Math.floor(Math.random()*3);
+  for(let i=0;i<4;++i){
+    if(i===blackIndex){
+      tiles.push(0);
+    }else{
+      tiles.push(1);
+    }
+  }
+}
+
+function drawTiles(){
+  for(let i=0;i<tiles.length;++i){
+    let xpos = (i%4)*tileWidth;
+    let ypos = Math.floor(i/4)*tileHeight;
+
+    ctx.fillStyle ='#000000'
+    if(tiles[i]==0){
+      ctx.fillRect(xpos,ypos,tileWidth,tileHeight);
+    }
+  }
+}
+
+addNewRow();
+addNewRow();
+addNewRow();
+addNewRow();
+drawTiles();
+console.log(tiles);
